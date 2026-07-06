b100e535a201b0c8406c575f9fc85b001dd58bb3
chambaDigital <yerctech@gmail.com>
2026-07-06 16:12:04 -0500

    fix: remove broken Google Photos URLs from gallery and change portfolio-grid-banner background-size to cover

--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -8,7 +8,6 @@ import { useState } from "react";
 
 // Google Photos extracted list & original images combined
 const ALL_IMAGES = [
-  // Original Webflow local images
   "/images/DJI_0033.jpg",
   "/images/DJI_0059.jpg",
   "/images/IMG_20230823_082703-EDIT_DxO_DxO-1918x1440.jpg",
@@ -21,32 +20,6 @@ const ALL_IMAGES = [
   "/images/IMG_20230821_093503-648x1440.jpg",
   "/images/IMG_20230822_162646-1920x1440.jpg",
   "/images/IMG_20230820_145404-811x1440.jpg",
-  // Google Photos Album
-  "https://lh3.googleusercontent.com/pw/AP1GczO5mjuLAQZPqB-kPOQXSF6w2JPXyzS-9-eDSYPXjqwbO_S0AeK2PccXgiiEBXQVOOlPn0_wCwER6ZlV4I5AgZk_aeExp3_DNfVl9PJeqeEG61XN2gVL",
-  "https://lh3.googleusercontent.com/pw/AP1GczO7Kd0hQeWkWUkQ6PzRp6ccTOBxW1Yc4I4C3OuWhRxi3IEABd3-v5GK2sA8sY0QtrZQaSBrTDfilJFhQXmb3JPGj2ECVb2EW0_EuldOLW-GFznyQ_pw",
-  "https://lh3.googleusercontent.com/pw/AP1GczN4gs9swfTj-fBjKbPomA8LJrDfsqG8xQ06UuoiRlt-yxKfEtqj8FD1-gyu-oJQ5X0UquWJfbLmhVngJQhdaD6h6ONo5L1YNjRUOWHOtot-QaBSZyeB",
-  "https://lh3.googleusercontent.com/pw/AP1GczNXBZpawgC8ASs2rZAazQuYXAsrjMkawkSOYyZY34eamV-7VvZjws_Y81ZW1SStil83C2inGM_fw6CqwzpowydQgy4UOVLsgiKDQgy85q5Kt8fWttDK",
-  "https://lh3.googleusercontent.com/pw/AP1GczNLJQNcAcL-iR-6dkqCHql46MUcDTha1oRVFX7OUX_GV_YCf-3jsAuPwg9RtI5D81swpDbXh1fxx7hd4bXvh0pYn7IPs95baG0ChgL2scV9_dzRG7TB",
-  "https://lh3.googleusercontent.com/pw/AP1GczNr4pmqe5rVM_lcxHGGAh4U0CSyBWdeSHcktKNkSIZ8HF3oQTc3wC28v8W7PEootgxm44xy2cdgxPX9tTwVfO0vVA_maTQTml8lua9iHjQ47-6VKV-l",
-  "https://lh3.googleusercontent.com/pw/AP1GczOfooxgEi5WqwVpCWBbQgcj239Fbdaylzf33KB6g1Xcg_MtB3yFOTgwFUdefd98SwUxX0PQ7dfNR2k_Ttb6aUf93NL2E8F03Y8kl5VjRczL-4u0PLaZ",
-  "https://lh3.googleusercontent.com/pw/AP1GczPlxPiv_pucKgPKohtBuA5nEm0gi-u5sYvReawq_Z2GkzCvBULjA6x-Ex7Q1o2xOsSjWRfJSNAxdDqDJarm6F4GLXBaeP5S9-dzvbMFHCb3bmT_-UaG",
-  "https://lh3.googleusercontent.com/pw/AP1GczOFAdvsTQ4afN8dP8Qr5i4HkIjk6Huo8CZIiL0mg_zUora8fvkPChN9EyZZGf_Y01ywQhyzVGhlPs5J0-lvMtqGlX644SSZvruM4ih1WxobZLhweP9A",
-  "https://lh3.googleusercontent.com/pw/AP1GczNjrxcTQQQUFAlLGjTVomQq7ZI8V74JqlFbTDFpPeiMCE3x8aAh8LtSXuLYOhIMMcprcnz2K4KSXVOqj3OWYGGc6QlYxipKai76w4wAuUqENYenDup4",
-  "https://lh3.googleusercontent.com/pw/AP1GczNwvqIhDDFa9MkQyuK7UqyiowPOtF-_QU6kuk7Y6FOXZq0_zm5rW_BB_JURE6ahImnBEpubxWMTdfxLma1ar94SNhhglVjr8ADIe0wW0vUfNG-3SMpz",
-  "https://lh3.googleusercontent.com/pw/AP1GczO_jmAe8rI09z1ssv2o7m28gz_DHFMo7Q3qMoWHbngTAncsYe0HxhQ-RXKAgLL4R6pJM-jLxfEsUdO70nWoSfQs65qdrS6vutW8lCC7uUsSEU2b5Z1E",
-  "https://lh3.googleusercontent.com/pw/AP1GczPDaOpU6AjLh--f-_H1pec2n8mEwpBLrcau--WZlkQKHCGfeF_oVB_qu5JF_ghP9bVBZYTNbmODuclQ-9RpURrD5VW45mditladexJsDbgE7fxvoiQC",
-  "https://lh3.googleusercontent.com/pw/AP1GczPpFgKSSNf47raz_pw43C68lFuXjIfvavXUtnYgQVtSYwMGRpydysbohQjqGzlbfyYFoaBd0jrMC0k5LHQyXPVauQPnr5upsTy8MAbR8oxANIINigRV",
-  "https://lh3.googleusercontent.com/pw/AP1GczPxJHibvAOy7VIckdi7SEO2AwVkWO3U5ghyHSlV3H82SiT9LZp1hb6KusJGfEd1UFI-cu0ilospfdlVtBnnzss2KjZwDsfqiyYOfhk80wLNMUgK-zkg",
-  // Google Photos Second Album (Rooms & Grounds)
-  "https://lh3.googleusercontent.com/pw/AP1GczMoacfVeB9nYb0JaJ1Hao5egci-FIJHHpq8MgwpSEso6mVZXrhyHtMSFmyzK6hnjcPVbwu7Oa4-R8U9HCtMajzBOXVTlqed_mtvm52PQmX5KitbW1q6",
-  "https://lh3.googleusercontent.com/pw/AP1GczORcSU53JUzkBxXnnLyaCPE8s0WkmgQdUkdFCKsWNtuzW4hMuXJeplnUQSk06fOA2IlfP_eklYeNot-dF8vRQFUK8jsBTsc6sTsk9wBna59W3efMmgp",
-  "https://lh3.googleusercontent.com/pw/AP1GczNrvH0aMU4SEGR5guPRn0-vK8DnrUUh3hX5Vi0cI-q_C7xuLmYZoxHkEklkaNsSdbU2YFdx_ehamgPhaAVPJ4MwqhlZB8mUmMBIxThvE2anxBmLtjvL",
-  "https://lh3.googleusercontent.com/pw/AP1GczN6YMnjoX-4-OI7AMmlfgaabQwsCuG1K2nficUkcIMcB9RfcjULl08EZaz7M4HrgX-78OilMXB_eJ_urDMqA8FsvTD-b_rQqbj374RnsHRmY3ZwDIoD",
-  "https://lh3.googleusercontent.com/pw/AP1GczOaHb1Lu2We0FnuD0VotSRgTV1e1U-rfjxiVwDQiaFj3sOmPnEcAWhH6HDCsaCemj_WEDdEoqIJFC3_Sg8EEqt9zwI2whIdLCVEBirJZrvNWxMXCU_r",
-  "https://lh3.googleusercontent.com/pw/AP1GczOicIvhfxdnSWpIRUoO37IjrC5Sul0tw32pINF3dE9SJluFDeXuDa2Ddola_zNT9BJbHMqsxS5aA3a10qvrW_EUKcGdMNHFfD2bzTJqKwQ8K9Oa0m_5",
-  "https://lh3.googleusercontent.com/pw/AP1GczMxT2DeZmZWRWuMjYL3vumGzQ7dV5YexWFYjMxEzIJJ0Mi5s_Io35_AG2LQ20DOLCWxbbotnzHPF71Q4Uik_tKbqPotBA3NA8-NMmkMbLdU9t8_4HH3",
-  "https://lh3.googleusercontent.com/pw/AP1GczOiuNfyiCDklFvO42r29elWmaWngIA0f8q9_T2Z0C9MiKp2n1aadoHAT0bFuKSLP5BZXMom9dOoxbVeSKqdiNuJp9Ia5zfFDCdfrcKI6DYryew1nPxe",
-  "https://lh3.googleusercontent.com/pw/AP1GczPO1HUFh72jesmG_WvDc3qGjeChKWf-rNRsH4fYHz1arYZG3Xk5QPxCGwaoBNaqR_NCfEOcsznR6x_xuQypXR6dfQIIURWwa2EjKiC5it83c7W4yGmS"
 ];
 
 export default function GaleriaPage() {
